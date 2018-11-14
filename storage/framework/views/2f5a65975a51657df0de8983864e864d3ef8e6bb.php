453f1736019365bd167128b8e2241b03aebafe74
Matthijs Schoneveld <44053796+matthijs76@users.noreply.github.com>
2018-11-14 12:18:31 +0100

    2

---
+++ storage/framework/views/2f5a65975a51657df0de8983864e864d3ef8e6bb.php
@@ -0,0 +1,42 @@
+<?php $__env->startSection('title'); ?>
+ Contact us
+ <?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="main">
+	  <H1 id="titel">Dikke Bertha koffie op kolen</H1>
+    
+      <div id="formulier">        
+        <div class= contact>
+            <?php if(Session::has('flash_message')): ?>
+            <div class="alert alert-succes"><?php echo e(Session::get('flash_message')); ?></div>
+            <?php endif; ?>
+            <form action="<?php echo e(route('contact.store')); ?>" method="post">
+                <?php echo e(csrf_field()); ?>
+
+                <input type="text" name="firstname" value="" placeholder="Naam">
+                <?php if($errors->has('firstname')): ?>
+                    <small class="form-text invalid-feedback"><?php echo e($errors->first('firstname')); ?></small>
+                <?php endif; ?>  <br><br>  
+                <input type="text" name="lastname" value="" placeholder="Achternaam">
+                <?php if($errors->has('lastname')): ?>
+                    <small class="form-text invalid-feedback"><?php echo e($errors->first('lastname')); ?></small>
+                <?php endif; ?>  <br><br>  
+                <input type="email" name="email" value="" placeholder="Uw email adres">
+                <?php if($errors->has('email')): ?>
+                    <small class="form-text invalid-feedback"><?php echo e($errors->first('email')); ?></small>
+                <?php endif; ?>   <br><br> 
+                <textarea name="message" rows="10" cols="30" placeholder="Uw vraag"></textarea>
+                <?php if($errors->has('message')): ?>
+                    <small class="form-text invalid-feedback"><?php echo e($errors->first('message')); ?></small>
+                <?php endif; ?>    
+                <br>
+                <input type="submit">
+            </form>
+        </div>    
+        </div>
+    </div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/6683e94212c3c38ef30cd61f8b3874c81c68e654.php
@@ -0,0 +1,19 @@
+ <?php $__env->startSection('content'); ?>
+<div class="main">
+	  <H1 id="titel">Agenda</H1>
+    
+    <h2 id="secTitel">Wij zijn te vinden op de volgende evenementen:</h2>
+    <p  style= "text-align: center;">
+    
+    <?php $__currentLoopData = $agendas; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $agenda): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <li id="agenda"><?php echo e($agenda->name); ?> </li>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    
+    
+    </p>
+      <div id="mainpic">
+      </div>
+      </div>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
