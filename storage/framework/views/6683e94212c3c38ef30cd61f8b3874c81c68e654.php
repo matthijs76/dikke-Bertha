f856afc9b2cc1af24703a37bc98e9c36f888da79
Matthijs Schoneveld <44053796+matthijs76@users.noreply.github.com>
2018-11-16 16:05:22 +0100

    versie 3 storage
    storage framework versie 3

--- storage/framework/views/6683e94212c3c38ef30cd61f8b3874c81c68e654.php
+++ storage/framework/views/6683e94212c3c38ef30cd61f8b3874c81c68e654.php
@@ -1,4 +1,5 @@
  <?php $__env->startSection('content'); ?>
+ 
 <div class="main">
 	  <H1 id="titel">Agenda</H1>
     
@@ -11,6 +12,36 @@
     
     
     </p>
+     <!-- Authentication Links -->
+     <?php if(auth()->guard()->guest()): ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo e(__('Login')); ?></a>
+                            </li>
+                            <li class="nav-item">
+                                <?php if(Route::has('register')): ?>
+                                    <a class="nav-link" href="<?php echo e(route('register')); ?>"><?php echo e(__('Register')); ?></a>
+                                <?php endif; ?>
+                            </li>
+                        <?php else: ?>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <?php echo e(Auth::user()->name); ?> <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="<?php echo e(route('logout')); ?>"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        <?php echo e(__('Logout')); ?>
+
+                                    </a>
+
+                                    <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" style="display: none;">
+                                        <?php echo csrf_field(); ?>
+                                    </form>
+                                </div>
+                            </li>
+                        <?php endif; ?>
       <div id="mainpic">
       </div>
       </div>
---
+++ storage/framework/views/78e09b18337fc09824b99eedafb8d772ac9d9d5d.php
@@ -0,0 +1,41 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    <?php if(session('status')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(session('status')); ?>
+
+                        </div>
+                    <?php endif; ?>
+
+                    You are logged in!<br><br>
+                    Upload hier je agendapunt:<br><hr>
+                    
+                    <?php echo Form::open(['action' => 'AgendaController@store', 'method' => 'POST']); ?>
+
+                        <div class="form-group">
+                           
+                            <?php echo e(Form::textarea('name','',['class' => 'form-control', 'placeholder' => 'Agendapunt'])); ?>
+
+                        </div>
+                        <?php echo e(Form::submit('Submit', ['class'=> 'btn btn-primary'])); ?>
+
+                    <?php echo Form::close(); ?>
+
+                </div>
+                
+                
+                       
+            </div>
+        </div>
+   
+</div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
